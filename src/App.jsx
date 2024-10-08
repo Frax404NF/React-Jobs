5233499088056c0b70bc9d75f60fd7c28cc4d73b
Frandi <frandicollage21@gmail.com>
2024-10-08 16:25:27 +0800

    finish landing page react job

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import JobsPage from './pages/JobsPage';
 import JobPage, { jobLoader } from './pages/Jobpage';
 import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
 
@@ -29,6 +30,18 @@ const App = () => {
     return;
   };
 
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements( 
       <Route path="/" element={ <MainLayout /> }>
@@ -36,6 +49,11 @@ const App = () => {
         <Route path="/jobs" element={ <JobsPage /> } />
         <Route path="/add-job" element={ <AddJobPage addJobSubmit={addJob}/> } />
         <Route 
+          path="/edit-job/:id" 
+          element={ <EditJobPage updateJobSubmit={updateJob}/> }
+          loader={jobLoader}
+        />
+         <Route 
           path="/jobs/:id" 
           element={ <JobPage deleteJob={deleteJob}/> }
           loader={jobLoader}
